028a6a6389f4d19bd1fd1c77099357d5953fc5e5
Arto Bendiken <arto@bendiken.net>
2010-04-07 05:47:48 +0200

    Defined a better 'markdown' alias.

--- .profile.d/python.sh
+++ .profile.d/python.sh
@@ -1,3 +1,4 @@
 export PATH=$PATH:/opt/local/Library/Frameworks/Python.framework/Versions/2.6/bin
 alias markdoc-init='markdoc init doc --vcs-ignore git'
 alias markdoc-build='markdoc build'
+alias markdown='markdown -x extra'
