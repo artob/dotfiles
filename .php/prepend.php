8ebfab59d1ea73f9262f92d608749f2349652bcd
Arto Bendiken <arto@bendiken.net>
2011-03-06 16:00:33 +0100

    Removed stupid copyright notices.

--- .php/prepend.php
+++ .php/prepend.php
@@ -2,10 +2,6 @@
 /**
  * ~/.php/prepend.php - Functions for php.ini's "auto_prepend_file" setting.
  *
- * @author Arto Bendiken <http://bendiken.net/>
- * @copyright Copyright (c) 2005-2007 Arto Bendiken. All rights reserved.
- * @license http://www.opensource.org/licenses/mit-license.php MIT
- * @license http://www.opensource.org/licenses/gpl-license.php GPL
  * @version 2008/12/07
  */
 
