4b02025648c4eac849f925d884e9e481037ca80e
Arto Bendiken <arto@bendiken.net>
2011-03-29 19:54:00 +0200

    Configured PKG_CONFIG_PATH for MacPorts.

--- .profile.d/macports.sh
+++ .profile.d/macports.sh
@@ -1,2 +1,6 @@
-export PATH=/opt/local/bin:/opt/local/sbin:$PATH
-export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig # for MacFUSE
+case $OSTYPE in
+  darwin*)
+    export PATH=/usr/local/bin:/opt/local/bin:/opt/local/sbin:$PATH
+    export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig:/opt/local/lib/pkgconfig:/usr/lib/pkgconfig:/usr/X11/lib/pkgconfig
+    ;;
+esac
