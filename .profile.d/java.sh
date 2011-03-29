338b78b21045bbfe5949401f2d6d49aa02dbe413
Arto Bendiken <arto@bendiken.net>
2011-03-29 19:44:55 +0200

    Ensured that JAVA_HOME is conditional on the system type.

--- .profile.d/java.sh
+++ .profile.d/java.sh
@@ -1,1 +1,9 @@
-export JAVA_HOME=/Library/Java/Home
+case $OSTYPE in
+  darwin*)
+    export JAVA_HOME=/Library/Java/Home
+    export JRUBY_HOME=/opt/local/share/java/jruby
+    ;;
+  linux*)
+    export JAVA_HOME=/usr/lib/jvm/java-6-openjdk # Ubuntu 10.10+
+    ;;
+esac
