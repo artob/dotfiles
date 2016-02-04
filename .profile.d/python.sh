11c37f68d76d6a59d154a5578c60dca5aa9f0d86
Arto Bendiken <arto@bendiken.net>
2016-02-04 10:45:12 +0100

    Ensured that OS X paths are contingent on $OSTYPE.

--- .profile.d/python.sh
+++ .profile.d/python.sh
@@ -1,4 +1,9 @@
-export PATH=$PATH:/opt/local/Library/Frameworks/Python.framework/Versions/2.6/bin
+case $OSTYPE in
+  darwin*)
+    export PATH=$PATH:/opt/local/Library/Frameworks/Python.framework/Versions/2.7/bin
+    ;;
+esac
+
 alias markdoc-init='markdoc init doc --vcs-ignore git'
 alias markdoc-build='markdoc build'
 alias markdown='markdown -x extra'
--- .profile.d/scala.sh
+++ .profile.d/scala.sh
@@ -1,2 +1,6 @@
-export SCALA_HOME=/opt/local/share/scala
-export USE_FSC=yes
+case $OSTYPE in
+  darwin*)
+    export SCALA_HOME=/opt/local/share/scala
+    export USE_FSC=yes
+    ;;
+esac
