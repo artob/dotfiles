b0e3a270cf400d8c8580b59884f4f3f39bf8a952
Arto Bendiken <arto@bendiken.net>
2017-06-29 13:49:49 +0200

    Ensured that `qmake` is found from the correct location.

--- .profile.d/qt.sh
+++ .profile.d/qt.sh
@@ -1,3 +1,3 @@
 if [ -d "$HOME/Library/Qt5.8.0/5.8/clang_64/bin" ]; then
-  export PATH="$PATH:$HOME/Library/Qt5.8.0/5.8/clang_64/bin"
+  export PATH="$HOME/Library/Qt5.8.0/5.8/clang_64/bin:$PATH"
 fi
