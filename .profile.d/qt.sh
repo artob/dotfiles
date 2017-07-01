bbd4f0ad5b87cf30d80b4cc431e429b1f7d372a4
Arto Bendiken <arto@bendiken.net>
2017-07-01 02:02:37 +0200

    Updated PATH for Qt 5.9.1.

--- .profile.d/qt.sh
+++ .profile.d/qt.sh
@@ -1,3 +1,3 @@
-if [ -d "$HOME/Library/Qt5.8.0/5.8/clang_64/bin" ]; then
-  export PATH="$HOME/Library/Qt5.8.0/5.8/clang_64/bin:$PATH"
+if [ -d "$HOME/Library/Qt5.9.1/5.9.1/clang_64/bin" ]; then
+  export PATH="$HOME/Library/Qt5.9.1/5.9.1/clang_64/bin:$PATH"
 fi
