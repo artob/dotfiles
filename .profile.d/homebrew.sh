654ffb0b93a99846458773de73db1572a4c3f4ed
Arto Bendiken <arto@bendiken.net>
2017-08-31 14:50:56 +0200

    Configured Homebrew.

--- .profile.d/homebrew.sh
+++ .profile.d/homebrew.sh
@@ -1,5 +1,10 @@
 case $OSTYPE in
   darwin*)
+    export HOMEBREW_CASK_OPTS='--appdir=~/Applications'
     export PATH=/opt/homebrew/bin:$PATH
+    export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig:/opt/homebrew/lib/pkgconfig:/usr/lib/pkgconfig:/usr/X11/lib/pkgconfig
+    export PKG_CONFIG_PATH=/opt/homebrew/opt/libffi/lib/pkgconfig:$PKG_CONFIG_PATH
+    export CPPFLAGS='-I/opt/homebrew/include'
+    export LDFLAGS='-L/opt/homebrew/opt/libffi/lib -L/opt/homebrew/lib'
     ;;
 esac
