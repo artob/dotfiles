b7a2fb699f3cbb3d74b38989872e891fdca5f63d
Arto Bendiken <arto@bendiken.net>
2017-08-31 14:51:46 +0200

    Uninstalled MacPorts.

--- .profile.d/macports.sh
+++
@@ -1,6 +0,0 @@
-case $OSTYPE in
-  darwin*)
-    export PATH=/usr/local/bin:/opt/local/bin:/opt/local/sbin:$PATH
-    export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig:/opt/local/lib/pkgconfig:/usr/lib/pkgconfig:/usr/X11/lib/pkgconfig
-    ;;
-esac
