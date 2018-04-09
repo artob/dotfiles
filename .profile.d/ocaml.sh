3175c25a75ed42a184d6485645ad86c239dc3f82
Arto Bendiken <arto@bendiken.net>
2018-04-09 13:25:53 +0300

    Configured OCaml and OPAM.

--- .profile.d/ocaml.sh
+++ .profile.d/ocaml.sh
@@ -1,1 +1,5 @@
-source "$HOME/.opam/opam-init/init.sh" > /dev/null 2>/dev/null || true
+#!/bin/sh
+if [ -f "$HOME/.opam/opam-init/init.sh" ]; then
+  # OPAM configuration
+  source "$HOME/.opam/opam-init/init.sh" > /dev/null 2> /dev/null || true
+fi
