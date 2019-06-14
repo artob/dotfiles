e8c75e4d4fece7c5d72b8e18fe1742ae646c6f88
Arto Bendiken <arto@bendiken.net>
2019-06-14 07:54:52 +0300

    Disabled obsolete JAVA_HOME configuration.

--- .profile.d/java.sh
+++ .profile.d/java.sh
@@ -1,9 +1,9 @@
 case $OSTYPE in
   darwin*)
-    export JAVA_HOME=/Library/Java/Home
-    export JRUBY_HOME=/opt/local/share/java/jruby
+    #export JAVA_HOME=/Library/Java/Home
+    #export JRUBY_HOME=/opt/local/share/java/jruby
     ;;
   linux*)
-    export JAVA_HOME=/usr/lib/jvm/java-6-openjdk # Ubuntu 10.10+
+    #export JAVA_HOME=/usr/lib/jvm/java-6-openjdk # Ubuntu 10.10+
     ;;
 esac
