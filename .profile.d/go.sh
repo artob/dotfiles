249b9ff4e8809cbeb9dd4c79caf4ca468540e55a
Arto Bendiken <arto@bendiken.net>
2019-06-24 14:21:54 +0300

    Ensured PATH includes GOPATH binaries.

--- .profile.d/go.sh
+++ .profile.d/go.sh
@@ -2,6 +2,6 @@ case $OSTYPE in
   darwin*)
     export GOPATH=/opt/go
     export GOROOT=/opt/homebrew/opt/go/libexec
-    export PATH="$PATH:$GOROOT/bin"
+    export PATH="$PATH:$GOROOT/bin:$GOPATH/bin"
     ;;
 esac
