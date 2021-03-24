96c4e2a33fbae0dc38a01f13eb58cb7f8bf43e31
Arto Bendiken <arto@bendiken.net>
2021-03-24 23:16:01 +0200

    Simplified the Homebrew configuration.

--- .profile.d/homebrew.sh
+++ .profile.d/homebrew.sh
@@ -1,10 +1,10 @@
 case $OSTYPE in
   darwin*)
-    export HOMEBREW_CASK_OPTS='--appdir=~/Applications'
-    export PATH=/opt/homebrew/bin:$PATH
-    export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig:/opt/homebrew/lib/pkgconfig:/usr/lib/pkgconfig:/usr/X11/lib/pkgconfig
-    export PKG_CONFIG_PATH=/opt/homebrew/opt/libffi/lib/pkgconfig:$PKG_CONFIG_PATH
-    export CPPFLAGS='-I/opt/homebrew/include'
-    export LDFLAGS='-L/opt/homebrew/opt/libffi/lib -L/opt/homebrew/lib'
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+    #export HOMEBREW_CASK_OPTS='--appdir=~/Applications'
+    #export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig:$HOMEBREW_PREFIX/pkgconfig:/usr/lib/pkgconfig:/usr/X11/lib/pkgconfig
+    #export PKG_CONFIG_PATH=$HOMEBREW_PREFIX/opt/libffi/lib/pkgconfig:$PKG_CONFIG_PATH
+    #export CPPFLAGS="-I$HOMEBREW_PREFIX/include"
+    #export LDFLAGS="-L$HOMEBREW_PREFIX/opt/libffi/lib -L$HOMEBREW_PREFIX/lib"
     ;;
 esac
