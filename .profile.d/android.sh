0df9358905fcc1ce152f200610b6b40e2d2439bf
Arto Bendiken <arto@bendiken.net>
2021-04-14 00:23:22 +0300

    Configured ANDROID_SDK_ROOT.

--- .profile.d/android.sh
+++ .profile.d/android.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 if [ -d "$HOME/Library/Android/sdk" ]; then
-  export ANDROID_HOME="$HOME/Library/Android/sdk"
-  export PATH="$PATH:$ANDROID_HOME/platform-tools"
+  export ANDROID_SDK_ROOT="$HOME/Library/Android/sdk"
+  export PATH="$PATH:$ANDROID_SDK_ROOT/platform-tools"
 fi
